80d7c3edc9c0ddb720c4a8ded31a4097a29b6205
D__G_UN <eorms96@naver.com>
2023-09-19 23:49:22 +0900

    Update 1등은 전부 나머지는 합하여서 리뷰순대로 출력.sql

--- Programmers/1등은 전부 나머지는 합하여서 리뷰순대로 출력.sql
+++ Programmers/1등은 전부 나머지는 합하여서 리뷰순대로 출력.sql
@@ -27,7 +27,7 @@ FROM
     MEMBER_PROFILE M
     ,(
     SELECT 
-        MAX(MEMBER_ID) MEMBER_ID
+        MEMBER_ID
         , SAME_COUNT
         , REVIEW_COUNT
     FROM
@@ -51,8 +51,68 @@ FROM
     ) COUNT_OF_SAME_RANK
 WHERE 
     M.MEMBER_ID = COUNT_OF_SAME_RANK.MEMBER_ID
-ORDER BY 1 DESC
+ORDER BY 1 DESC, 2 ASC
 ;
+
+
+
+-- SELECT 
+--     MOST.CNT CNT
+--     , M.MEMBER_NAME REVIEWER
+-- FROM 
+--    MEMBER_PROFILE M
+--    ,(
+--     SELECT 
+--         MEMBER_ID, 
+--         COUNT(*) CNT, 
+--         RANK() OVER(ORDER BY COUNT(*) DESC) RNK
+--     FROM REST_REVIEW
+--     GROUP BY MEMBER_ID
+--     ) MOST
+-- WHERE
+--     M.MEMBER_ID = MOST.MEMBER_ID
+--     AND RNK=1
+-- GROUP BY 
+--     MEMBER_NAME
+-- UNION ALL
+-- SELECT
+--     REVIEW_COUNT
+--     ,CASE 
+--         WHEN COUNT_OF_SAME_RANK.SAME_COUNT>1 THEN CONCAT(M.MEMBER_NAME," 외 ",COUNT_OF_SAME_RANK.SAME_COUNT," 명") 
+--         ELSE M.MEMBER_NAME
+--     END REVIEWER
+-- FROM
+--     MEMBER_PROFILE M
+--     ,(
+--     SELECT 
+--         MAX(MEMBER_ID) MEMBER_ID
+--         , SAME_COUNT
+--         , REVIEW_COUNT
+--     FROM
+--         (
+--         SELECT 
+--             MEMBER_ID
+--             , COUNT(*) OVER(PARTITION BY REVIEW_COUNT) SAME_COUNT
+--             , REVIEW_COUNT
+--         FROM
+--             (
+--             SELECT 
+--                 MEMBER_ID
+--                 , COUNT(*) REVIEW_COUNT
+--                 , RANK() OVER(ORDER BY COUNT(*) DESC) RNK
+--             FROM REST_REVIEW
+--             GROUP BY MEMBER_ID
+--             ) t
+--         WHERE RNK > 1
+--         ) t
+--         GROUP BY REVIEW_COUNT
+--     ) COUNT_OF_SAME_RANK
+-- WHERE 
+--     M.MEMBER_ID = COUNT_OF_SAME_RANK.MEMBER_ID
+-- ORDER BY 1 DESC
+-- ;
+
+
 
 -- SELECT 
 --     MOST.CNT CNT
