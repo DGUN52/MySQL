4329b6f7299b7f4894ba2762302e5e4ba62ad616
D__G_UN <eorms96@naver.com>
2023-11-24 01:07:13 +0900

    Update Chanllenges.sql

--- HackerRank/Chanllenges.sql
+++ HackerRank/Chanllenges.sql
@@ -1,3 +1,54 @@
+# 최적화
+select 
+    hacker_id
+    , name
+    , total_number
+from 
+    (
+        select 
+            h.hacker_id
+            , h.name
+            , count(*) total_number
+            , rank() over(order by count(h.hacker_id) desc) rnk
+        from
+            hackers h
+            , challenges c
+        where 
+            h.hacker_id = c.hacker_id
+        group by 
+            h.hacker_id, h.name
+    ) handc
+where 
+    rnk = 1
+union all
+select 
+    hacker_id
+    , name
+    , total_number
+from
+    (    
+        select
+            h.hacker_id
+            , h.name
+            , count(*) total_number
+            , dense_rank() over(order by count(*) desc) rnk
+            , count(count(*)) over(partition by count(*)) cnt
+        from
+            hackers h
+            , challenges c
+        where 
+            h.hacker_id = c.hacker_id
+        group by 
+            h.hacker_id, h.name
+    ) hc
+where 
+    cnt = 1
+    and rnk > 1
+order by 
+    3 desc
+    , 1
+;
+/*
 select 
     h.hacker_id
     , h.name
@@ -67,3 +118,4 @@ order by
     3 desc
     , 1
 ;
+*/
