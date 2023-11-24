a94a3863772fc6e97dffcbba264d75dbf63ab3c8
D__G_UN <eorms96@naver.com>
2023-11-24 21:21:12 +0900

    Update Contest Leaderboard.sql

--- HackerRank/Contest Leaderboard.sql
+++ HackerRank/Contest Leaderboard.sql
@@ -1,4 +1,31 @@
 # https://velog.io/@deaf52/HackerRank-SQL%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-lv.4-Challenges
+select
+    hacker_id
+    , name
+    , sum(max_score) total_score
+from
+    hackers h
+join (
+    select
+        s.hacker_id
+        , s.challenge_id
+        , max(s.score) max_score
+    from
+        submissions s
+    group by
+        s.hacker_id
+        , s.challenge_id
+    having
+        max(s.score) > 0
+    ) t on h.hacker_id = t.hacker_id
+group by
+    hacker_id
+    , name
+order by
+    3 desc,
+    1 asc;
+
+/*    
 select 
     hacker_id
     , name
@@ -27,3 +54,4 @@ order by
     3 desc
     , 1 asc
 ;
+*/
