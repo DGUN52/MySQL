1b5be1baea2878bbad4ab0e679e6d5e81a3bbcf8
D__G_UN <eorms96@naver.com>
2023-12-11 23:30:57 +0900

    Create Symmetric Pairs Discussions.sql

---
+++ HackerRank/Symmetric Pairs Discussions.sql
@@ -0,0 +1,22 @@
+select *
+from (
+    select
+        if(f1.x < f1.y, f1.x, null) x
+        , if(f1.x < f1.y, f1.y, null) y
+    from 
+        functions f1
+        , functions f2
+    where
+        f1.x = f2.y and f1.y = f2.x
+    ) t
+where 
+    t.x is not null 
+    and t.y is not null    
+union all
+select *
+from functions
+where x = y
+group by x, y
+having count(*) > 1
+order by 1
+;
