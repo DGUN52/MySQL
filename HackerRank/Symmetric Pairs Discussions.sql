9d0a606bfb1e972a222c8d39b40924dff6581872
D__G_UN <eorms96@naver.com>
2023-12-11 23:33:12 +0900

    Update Symmetric Pairs Discussions.sql

--- HackerRank/Symmetric Pairs Discussions.sql
+++ HackerRank/Symmetric Pairs Discussions.sql
@@ -1,17 +1,12 @@
-select *
-from (
-    select
-        if(f1.x < f1.y, f1.x, null) x
-        , if(f1.x < f1.y, f1.y, null) y
-    from 
-        functions f1
-        , functions f2
-    where
-        f1.x = f2.y and f1.y = f2.x
-    ) t
-where 
-    t.x is not null 
-    and t.y is not null    
+select
+    f1.x
+    , f1.y
+from 
+    functions f1
+    , functions f2
+where
+    (f1.x = f2.y and f1.y = f2.x)
+    and f1.x < f1.y
 union all
 select *
 from functions
@@ -20,3 +15,24 @@ group by x, y
 having count(*) > 1
 order by 1
 ;
+
+-- select *
+-- from (
+--     select
+--         if(f1.x < f1.y, f1.x, null) x
+--         , if(f1.x < f1.y, f1.y, null) y
+--     from 
+--         functions f1
+--         , functions f2
+--     where
+--         f1.x = f2.y and f1.y = f2.x
+--     ) t
+-- where t.x is not null and t.y is not null    
+-- union all
+-- select *
+-- from functions
+-- where x = y
+-- group by x, y
+-- having count(*) > 1
+-- order by 1
+-- ;
